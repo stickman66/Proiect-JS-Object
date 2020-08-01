92e1b81a347abaa7a6179094ed2a0263f66683ec
stickman66 <54074904+stickman66@users.noreply.github.com>
2020-08-01 15:44:43 +0300

    Add files via upload

---
+++ proiect/src/config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const SELECT_PRODUCT = "select-produs";
+
+const ID_PRODUCT_TEMPLATE = "#product-template";
+const ID_COS_TEMPLATE = "#item-basket-template";
+const ID_DATA = "#data";
+const ID_SORTARE = "#sortare";
+const ID_ITEMS = "#items";
+const ID_TOTAL_COS = "#total";
+
+const CLS_IMAGE = ".image";
+const CLS_NAME = ".name";
+const CLS_PRET = ".price";
+const CLS_ADD = ".add";
+const CLS_CANTITATE = ".cantitate";
+const CLS_TOTAL = ".total";
+const CLS_REMOVE = ".remove";
+
---
+++ proiect/src/produs.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class View{
+    constructor(){
+        this.produsFragment = document.querySelector(ID_PRODUCT_TEMPLATE).content;
+        this.produse = document.querySelector(ID_DATA)
+        
+    }
+    update(items){
+        //this.produse.textContent = "";
+
+        const fragment = document.createDocumentFragment();
+        for ( let i of items){
+
+            let item = this.produsFragment.cloneNode(true);
+
+            item.querySelector(".name").textContent = i.name;
+            item.querySelector(".price").textContent = i.price;
+            item.querySelector(".image").src = i.image;
+
+            fragment.appendChild(item);
+        }
+        this.produse.appendChild(fragment);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () =>     {
+        new View();
+        console.log("!")
+})
